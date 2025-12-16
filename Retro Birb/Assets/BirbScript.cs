81f5a721738a2ce81974b98c58edde66822df237
DevadharsanVS <224190116+YeetusGalactus@users.noreply.github.com>
2025-12-16 13:09:05 -0800

    Added restart feature and build

--- Retro Birb/Assets/BirbScript.cs
+++ Retro Birb/Assets/BirbScript.cs
@@ -1,21 +1,31 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BirbScript : MonoBehaviour
 {
     public Rigidbody2D birdbody;
     public float jumpForce;
+    public LogicScript logic;
+    public bool ballActive = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) == true && ballActive)
         {
             birdbody.linearVelocity = Vector2.up * jumpForce;
         }
+
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        ballActive = false;
+    }
+
 }
--- Retro Birb/Assets/LogicScript.cs
+++ Retro Birb/Assets/LogicScript.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class LogicScript : MonoBehaviour
 {
     public int playerScore;
     public Text scoreText;
+    public GameObject gameOverScreen;
 
     [ContextMenu("Increase Score")]
     public void addScore(int scoreToAdd)
@@ -12,4 +14,13 @@ public class LogicScript : MonoBehaviour
         scoreText.text = playerScore.ToString();
     }
 
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
+    }
 }
