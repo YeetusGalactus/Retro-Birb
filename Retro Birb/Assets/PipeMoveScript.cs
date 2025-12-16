e67dc48f02dd9776b6eab8c9dd9cddf1b3e37375
DevadharsanVS <224190116+YeetusGalactus@users.noreply.github.com>
2025-12-16 12:04:48 -0800

    Changed assets and pipe scripts
    Changed assets for the game including the player sprite and pipe sprite. Added new scripts for spawning, moving, and destroying the pipes.

---
+++ Retro Birb/Assets/PipeMoveScript.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PipeMoveScript : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    public float deadZone = -3;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += (Vector3.left * moveSpeed) * Time.deltaTime;
+
+        if (transform.position.x < deadZone){
+            Debug.Log("deleted Pipe object");
+            Destroy(gameObject);
+        }
+    }
+}
